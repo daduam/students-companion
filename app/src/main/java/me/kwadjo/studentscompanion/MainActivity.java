c082e8d16db3ec8e9d79914b1bc5a01e3a876f74
daduam <josephampadu549@gmail.com>
2021-09-30 20:26:56 +0000

    sommeil

--- app/src/main/java/me/kwadjo/studentscompanion/MainActivity.java
+++ app/src/main/java/me/kwadjo/studentscompanion/MainActivity.java
@@ -4,17 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 import android.widget.ArrayAdapter;
-
+    
 import com.google.android.material.slider.Slider;
-import com.google.android.material.sldier.LabelFormatter;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String[] colleges = { "College of Health Sciences", "College of Basic and Applied Sciences",
-            "College of Humanities", "College of Education" };
+    private static final String[] colleges = {"College of Health Sciences", "College of Basic and Applied Sciences", "College of Humanities", "College of Education"};
 
     EditText mName;
     EditText mIndexNumber;
@@ -27,37 +29,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mName = findViewById(R.id.form_name_input).editText;
-        mIndexNumber = findViewById(R.id.form_index_number_input).editText;
-        mCollege = (AutoCompleteTextView) findViewById(R.id.form_college_spinner).editText;
+        mName = findViewById(R.id.form_name_input);
+        mIndexNumber = findViewById(R.id.form_index_number_input);
+        mCollege = findViewById(R.id.form_college_spinner);
         mGpaSlider = findViewById(R.id.form_gpa_slider);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.list_item, colleges);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item, colleges);
         mCollege.setAdapter(adapter);
 
-        mGpaSlider.setLabelFormatter(new LabelFormatter() {
-            @NonNull
-            @Override
-            public String getFormattedValue(float value) {
-                DecimalFormat df = new DecimalFormat("0.00");
-                return df.format(value);
-            }
+        mGpaSlider.setLabelFormatter(value -> {
+            DecimalFormat df = new DecimalFormat("0.00");
+            return df.format(value);
         });
     }
 
     public void saveDetails(View view) {
-        StringBuilder sb = new StringBuilder("");
+        StringBuilder sb = new StringBuilder();
 
         String name = mName.getText().toString();
         String indexNumber = mIndexNumber.getText().toString();
         String college = colleges[mCollege.getListSelection()];
-        String gpa = mGpaSlider.getValue();
+        String gpa = String.valueOf(mGpaSlider.getValue());
 
-        sb.append("Name: " + name + "\n");
-        sb.append("Index Number: " + indexNumber + "\n");
-        sb.append("College: " + college + "\n");
-        sb.append("Level: " + level + "\n");
-        sb.append("GPA: " + gpa + "\n");
+        sb.append("Name: ").append(name).append("\n");
+        sb.append("Index Number: ").append(indexNumber).append("\n");
+        sb.append("College: ").append(college).append("\n");
+        sb.append("Level: ").append(level).append("\n");
+        sb.append("GPA: ").append(gpa).append("\n");
 
         Toast.makeText(this, sb.toString(), Toast.LENGTH_LONG).show();
     }
@@ -65,23 +63,17 @@ public class MainActivity extends AppCompatActivity {
     public void setLevel(View view) {
         boolean checked = ((RadioButton) view).isChecked();
 
-        switch (view.getId()) {
-            case R.id.form_level_100:
-                if (checked)
-                    this.level = "100";
-                break;
-            case R.id.form_level_200:
-                if (checked)
-                    this.level = "200";
-                break;
-            case R.id.form_level_300:
-                if (checked)
-                    this.level = "300";
-                break;
-            case R.id.form_level_400:
-                if (checked)
-                    this.level = "400";
-                break;
+        int viewId = view.getId();
+        if (checked) {
+            if (viewId == R.id.form_level_100) {
+                this.level = "100";
+            } else if (viewId == R.id.form_level_200) {
+                this.level = "200";
+            } else if (viewId == R.id.form_level_300) {
+                this.level = "300";
+            } else if (viewId == R.id.form_level_400) {
+                this.level = "400";
+            }
         }
     }
 }
